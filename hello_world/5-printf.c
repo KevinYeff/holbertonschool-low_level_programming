570aa50e3a7a841b4a589ea9daf0d5a900da129f
KevinYeff <kevinyeff94@gmail.com>
2022-09-29 00:27:18 -0500

    feat: Script 5. Hello, printf. Script that prints a given string, followed ny a new line.

---
+++ hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * void is a good practice.
+ * The printf() function sends a formatted string
+ * to the standard output (the display)
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints a given string.
+ *
+ * Return: (0).
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
