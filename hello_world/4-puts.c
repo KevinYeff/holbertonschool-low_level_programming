cf9769ae89a39d89e99636e978df553566e99bda
KevinYeff <kevinyeff94@gmail.com>
2022-09-29 00:03:58 -0500

    feat: Script 4. Hello, puts. Script that prints a given string, followed by a new line.

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * void is a good practice.
+ * puts command: similar to printf() function.
+ * writes the string s and a trailing newline to stdout.
+ * main - prints "Programming is like building a multilingual puzzle.
+ * Return: (0).
+ **/
+
+int main (void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
