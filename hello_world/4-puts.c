deb14c713975cfbd73667601cf1bbb104414a60f
KevinYeff <kevinyeff94@gmail.com>
2022-09-29 00:15:05 -0500

    fix: Betty style applied.

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,18 @@
-#include <stdio.h>
-/**
+/*
  * void is a good practice.
  * puts command: similar to printf() function.
  * writes the string s and a trailing newline to stdout.
+ */
+
+#include <stdio.h>
+
+/**
  * main - prints "Programming is like building a multilingual puzzle.
  * Return: (0).
- **/
+ */
 
-int main (void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
