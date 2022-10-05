1db45c318ed3351f5279863ee5ec8d5d7dc3c8f5
KevinYeff <kevinyeff94@gmail.com>
2022-10-04 22:13:58 -0500

    feat: C program 11. 98 Battery Street, the OG.

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all natural numbers
+ *               from n to 98, followed by a
+ *               newline.
+ * @n: The integer.
+ *
+ * Return: (0).
+ */
+void print_to_98(int n)
+{
+	n = n;
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	printf("%d\n", n);
+}
