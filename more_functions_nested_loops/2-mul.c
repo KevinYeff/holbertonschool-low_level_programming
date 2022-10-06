da001b0e5c0605ce524eb9f6e4da358add5e70a6
KevinYeff <kevinyeff94@gmail.com>
2022-10-06 14:42:22 -0500

    fix: Blank line after declaration added, description for return value added.

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -3,9 +3,11 @@
  * mul - Function that multiplies two integers.
  * @a: First integer.
  * @b: Second integer.
+ * Return: returns the result.
  */
 int mul(int a, int b)
 {
 	int m = a * b;
+
 	return (m);
 }
