4ee1d302c38df8dd83645a31cbfa43bb0ebd5752
KevinYeff <kevinyeff94@gmail.com>
2022-10-07 21:35:53 -0500

    fix: Correct description on comment added.

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * more_numbers - Dunction that prints 10 times the numbers, from 0 to 14,
- *                followed by a new line.
+ * more_numbers - Function that prints 10 times the numbers,
+ *                from 0 to 14, followed by a new line.
  */
 void more_numbers(void)
 {
