6b3dc158075fdd317bb0d9c731e916cf076a74ea
KevinYeff <kevinyeff94@gmail.com>
2022-10-09 17:00:05 -0500

    feat: This C program is the solution to the advanced task number 13.

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Program that computes and prints the sum of all multipes of
+ *        3 or 5 below 1024 (excluded); followed by a new line.
+ * Return: (0).
+ */
+int main(void)
+{
+	int num, sum;
+
+	num = 0;
+	sum = 0;
+	while (num < 1024)
+	{
+		if (sum % 3  == 0 && sum % 5 == 0)
+		{
+			sum = sum += num;
+		}
+		num++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
