d625a3cbb32322a836a034e35118a9169d673bb2
KevinYeff <kevinyeff94@gmail.com>
2022-10-09 20:27:41 -0500

    feat: C program that solves the advanced task 15

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Program that finds and prints the sum of the even-valued terms,
+ *        followed by a new line.
+ *        considering the terms in the Fibonacci sequence whose values do
+ *        not exceed 4,000,000.
+ * Return: (0).
+ */
+int main(void)
+{
+	unsigned long num1, num2, sum, eva;
+
+	num1 = 0, num2 = 1, eva = 0;
+	while (1)
+	{
+		sum = num1 += num2;
+		num1 = num2;
+		num2 = sum;
+		if (num2 > 4000000)
+			break;
+		if (num2 <= 4000000 && num2 % 2 == 0)
+			eva = eva += num2;
+	}
+	printf("%ld\n", eva);
+	return (0);
+}
