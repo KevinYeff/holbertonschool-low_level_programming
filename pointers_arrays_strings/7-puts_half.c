abe0c3c5dd922de940b51ca94232987d2d75b824
KevinYeff <kevinyeff94@gmail.com>
2022-10-11 17:12:52 -0500

    feat: C program that prints half of a string.

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,46 @@
+#include "main.h"
+int _strlen(char *);
+/**
+ * puts_half - Function that prints half of a string,
+ *             followed by a new line.
+ * @str:       Pointer, type char, string.
+ */
+void puts_half(char *str)
+{
+	int len = _strlen(str), index;
+	char *half;
+
+	index = 0;
+	while (index < len / 2)
+	{
+		str++;
+		index++;
+	}
+	half = str;
+	while (half)
+	{
+		_putchar(*str);
+		half++;
+		str++;
+		if (*str == '\0')
+			break;
+	}
+	_putchar(10);
+}
+
+/**
+ * _strlen - Function that returns the length of a string.
+ * @s:       Pointer, type char.
+ * Return:   A variable that contains the lenght of the string.
+ */
+int _strlen(char *s)
+{
+	int go = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		go++;
+	}
+	return (go);
+}
