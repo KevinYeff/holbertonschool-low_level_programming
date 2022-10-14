4f20d022a734546de713d055b18bb9bf29db2b67
KevinYeff <kevinyeff94@gmail.com>
2022-10-14 03:01:48 -0500

    feat: C program that compares two strings.

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcmp - Function that compares two strings.
+ * @s1:      Pointer, type char.
+ * @s2:      Pointer, type char.
+ * Return:   difference or equality between strings.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s1 == *s2)
+	{
+		if (*s1 == *s2)
+			return (0);
+	}
+	return (*s1 - *s2);
+}
