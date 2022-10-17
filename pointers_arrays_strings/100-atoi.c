9ba9d824160c8c7c395dcbc0ece6d08312f17b12
KevinYeff <kevinyeff94@gmail.com>
2022-10-16 23:50:05 -0500

    feat: C program that converts a string into integers.

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _atoi - Function that convert a string to an integer.
+ * @s:     String.
+ * Return: Integers.
+*/
+int _atoi(char *s)
+{
+	int inx, atoi, sign;
+
+	atoi = 0;
+	inx = 0;
+	sign = 1;
+
+	while (!(*(s + inx) >= 48 && (*(s + inx) <= 57)) && *(s + inx) != '\0')
+	{
+		if (*(s + inx) == 45)
+		{
+			sign *= -1;
+		}
+		inx++;
+	}
+
+	while ((*(s + inx) >= 48) && (*(s + inx) <= 57) && *(s + inx) != '\0')
+	{
+		atoi = atoi * 10 + (*(s + inx) - '0');
+		inx++;
+
+		if (*(s + inx) == '\0')
+			break;
+	}
+	return (atoi * sign);
+}
