80e914fe8e09a7dcfe32e71a3803b8a11c80e079
KevinYeff <kevinyeff94@gmail.com>
2022-10-16 19:38:31 -0500

    update: strchr prototype added

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -24,3 +24,4 @@ char *leet(char *);
 /* even more pointers */
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
