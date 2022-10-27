2607afd26bfba52b8595a5b5695f86c28e3bc9ed
KevinYeff <kevinyeff94@gmail.com>
2022-10-27 01:29:51 -0500

    fix: line 22, size of type int modified.

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	matrix = (int **)malloc(sizeof(int) * width);
+	matrix = (int **)malloc(sizeof(int *) * width);
 
 	if (!matrix)
 		return (NULL);
