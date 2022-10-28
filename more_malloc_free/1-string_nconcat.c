ab7d59e279a1d5e26f4919f6dbe43f56f68939ad
KevinYeff <kevinyeff94@gmail.com>
2022-10-28 00:50:27 -0500

    feat: C - program that concatenates two strings, all the string number 1 and n bytes of the second string.

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *string_nconcat - Function that concatenates two strings.
+ * @s1:              First string.
+ * @s2:              Second string.
+ * @n :              Number of characters to take from string number 2.
+ * Return:           A pointer to a newly allocated spae in memory.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *allocate;
+	unsigned int totalLen, i;
+
+	if (!s1)
+		return (NULL);
+	if (!s2)
+		return (NULL);
+
+	totalLen = n;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	totalLen += i;
+
+	allocate = (char *)malloc(sizeof(int) * (totalLen + 1));
+
+	if (allocate == NULL)
+		return (NULL);
+
+	for (i = 0; *s1 != '\0'; i++)
+		*allocate++ = *s1++;
+
+	for (i = 0; *s2 && i < n; i++)
+		*allocate++ = *s2++;
+
+	*allocate = '\0';
+
+	return (allocate - totalLen);
+}
