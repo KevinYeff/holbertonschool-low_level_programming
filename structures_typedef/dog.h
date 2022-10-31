32126e00f69421a688eb48a991485ccedff5ced3
KevinYeff <kevinyeff94@gmail.com>
2022-10-31 00:42:35 -0500

    Header file, task 0, defines a struct named dog.

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_DOG
+#define HEADER_DOG
+
+/**
+ * struct dog - Struc that contains the following elements.
+ *
+ * @name:       Name of some dog.
+ * @age:        Age of a dog.
+ * @owner:      Dog owner name.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
