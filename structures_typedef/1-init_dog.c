53c2d026cd46093cc7d2d995af46659fd25f1e1d
KevinYeff <kevinyeff94@gmail.com>
2022-10-31 01:22:06 -0500

    feat: C program that initialize a struct.

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - Function that initialize a variable of type struct dog
+ *
+ * @d:        Pointer to elements of my structure.
+ * @name:     Name of some dog.
+ * @age:      Age ofthe dog.
+ * @owner:    Dog owner name.
+ * Return:    void.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name;
+		d->age;
+		d->owner;
+	}
+	else
+		return;
+}
