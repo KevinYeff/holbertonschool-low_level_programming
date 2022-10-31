a226f4e3ce70bc06b95f63ccf7ae69a556fc3654
KevinYeff <kevinyeff94@gmail.com>
2022-10-31 17:07:38 -0500

    update: array_iterattor prototype added.

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTION_P
 #define FUNCTION_P
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
