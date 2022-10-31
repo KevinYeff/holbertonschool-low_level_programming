027ab89442d4fd6e343463c13d88da5dc618345a
KevinYeff <kevinyeff94@gmail.com>
2022-10-31 17:42:51 -0500

    update: prototype for get_op_func.c file added.

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,6 +1,7 @@
 #ifndef CALC_H
 #define CALC_H
 
+/* struct op */
 /**
  * struct op - Struct op
  *
@@ -19,5 +20,8 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+
+/* get_op_func.c */
+int (*get_op_func(char *s))(int, int);
 
 #endif
