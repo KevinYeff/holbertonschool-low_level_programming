ce528cee8f9c090e0c1d228c87163dceecc1b799
KevinYeff <kevinyeff94@gmail.com>
2022-11-16 20:59:09 -0500

    feat: C program that frees a listint_t list.

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * free_listint - Function that frees a listint_t list.
+ * @head:         Head of the list.
+ * Return:        Void.
+ */
+void free_listint(listint_t *head)
+{
+	if (!head)
+		return;
+	free_listint(head->next);
+	free(head);
+}
