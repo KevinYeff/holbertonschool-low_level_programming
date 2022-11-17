6a180c065066f689a1a6096c67b0b4d538a539d6
KevinYeff <kevinyeff94@gmail.com>
2022-11-16 21:19:08 -0500

    feat: C program that frees a lists_t list.

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - A function that free a listint_t list.
+ * @head:          A pointer to a pointer to struct of lintint_t.
+ * Return:         Nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (!head)
+		return;
+	while (*head)
+	{
+		temp = *head;
+		*head = temp->next;
+		free(temp);
+	}
+}
