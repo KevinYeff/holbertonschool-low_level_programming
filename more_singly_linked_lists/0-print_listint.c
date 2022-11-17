82ede1723aad4a40936422c033e773252dc4e0e7
KevinYeff <kevinyeff94@gmail.com>
2022-11-16 19:15:39 -0500

    feat: C program that prints all the elements of a list.

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_list - Function that prints all the elements of a list_l list.
+ * @h:          Pointer to a Linked list.
+ * Return:      Number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t n;
+
+	if (!h)
+		return (0);
+	n = 0;
+	while(h)
+	{
+		n++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (n);
+}
