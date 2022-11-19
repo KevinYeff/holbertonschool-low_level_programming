67ae1fb6e7291dfe3c9fa6a86eef3d426828cb10
KevinYeff <kevinyeff94@gmail.com>
2022-11-19 17:58:33 -0500

    feat: C program that prints the binary representation of a number.

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Function that converts abinary number to an unsigned int.
+ * @b:              Pointer to string.
+ * Return:          The converted number.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec = 0;
+	int i = 0;
+
+	if (*b == '\0')
+		return (0);
+
+	i = 0;
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		b++;
+		i++;
+	}
+	b -= i;
+
+	i = 0;
+	while (*b != '\0')
+	{
+		if (*b == '1')
+			dec = (dec << 1) | 1;
+		else if (*b == '0')
+			dec <<= 1;
+		b++;
+		i++;
+	}
+	b -= i;
+	return (dec);
+}
