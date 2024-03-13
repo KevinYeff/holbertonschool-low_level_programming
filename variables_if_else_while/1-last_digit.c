499fa66f7e763f47a76ec9d84b506316414454ad
KevinYeff <kevinyeff94@gmail.com>
2024-03-13 00:51:18 -0500

    doc: Comments to explain the code were added

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,20 +2,24 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Prints a random number whether is positive, negative or zero and its
- * last digit.
+ * main - Prints The last digit of a random number whether is positive,
+ *        negative or zero.
  *
  * Return: (0).
  */
 
 int main(void)
 {
+	/*Declares variables*/
 	int n, ld;
-
+	/* Sets the seed, uses current time */
 	srand(time(0));
+	/* Saves pseudo-random number to variable n*/
 	n = rand() - RAND_MAX / 2;
+	/* Extracts the last digit and saves it to variable ld */
 	ld = n % 10;
 
+	/* Conditionals behaviors */
 	if (ld > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	else if (ld < 6 && ld != 0)
@@ -25,5 +29,6 @@ int main(void)
 	}
 	else
 		printf("Last digit of %d is %d and is 0\n", n, ld);
+	/* Ends */
 	return (0);
 }
