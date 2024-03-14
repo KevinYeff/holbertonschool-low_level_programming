045719fa675f8a2cdaaf7c5e16e3e673ffac21e9
KevinYeff <kevinyeff94@gmail.com>
2024-03-14 12:58:51 -0500

    refactoring: Code comments added, now uses for loop instead.

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 /**
  * main - Prints the alphabet in lowercae , and uppercase.
+ *        We can use ASCII values too.
  *
  * Return: (0).
  */
 
 int main(void)
 {
+	/* Declares variables */
 	char alphalow, alphaupp;
-
+	/* Sets values to the variables */
 	alphalow = 'a';
 	alphaupp = 'A';
-
-	while (alphalow <= 'z')
-	{
+	/* loops based on conditions */
+	for (; alphalow <= 'z'; alphalow++)
 		putchar(alphalow);
-		alphalow++;
-	}
-	while (alphaupp <= 'Z')
-	{
+
+	for (; alphaupp <= 'Z'; alphaupp++)
 		putchar(alphaupp);
-		alphaupp++;
-	}
+	/* Adds new line */
 	putchar('\n');
+	/* Ends  */
 	return (0);
 }
