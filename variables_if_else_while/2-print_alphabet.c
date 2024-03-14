42f5ec9799e1cfaeaa4903e22bd0482d66c4d4e2
KevinYeff <kevinyeff94@gmail.com>
2024-03-13 19:35:09 -0500

    refactoring: added comments, changed while to for loop, also a small explanation in the doc

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 /**
- * main - Prints the alphabet in lower case.
+ * main - Prints the alphabet in lower case
+ *        uses ASCII Values.
  *
  * Return: (0).
  */
 
 int main(void)
 {
-	char alphalow;
+	/* Declares var type char */
+	char currentLetter;
 
-	alphalow = 'a';
-
-	while (alphalow <= 'z')
-	{
-		putchar(alphalow);
-		alphalow++;
-	}
-	putchar('\n');
+	/* Iterates through the ascii values from a to z */
+	for (currentLetter = 97; currentLetter <= 122; currentLetter++)
+		/* Prints the current letter till break the condition */
+		putchar(currentLetter);
+	/* Prints a new line */
+	putchar(10);
+	/* Ends */
 	return (0);
 }
