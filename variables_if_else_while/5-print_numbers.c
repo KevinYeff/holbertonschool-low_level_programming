a54aa62bd2abc43c77465775d270ade17b50ee4d
KevinYeff <kevinyeff94@gmail.com>
2024-03-15 00:43:33 -0500

    refactoring: Comments added, function definition improved

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 /**
  * main - Prints numbers of base 10.
+ *        Uses char representation of integers.
  *
  * Return: (0).
  */
 
 int main(void)
 {
-	char numbers;
-
-	numbers = '0';
-
-	while (numbers <= '9')
-	{
+	/* Declare char variable */
+	char numbers = '0';
+	/* Loop  */
+	for (; numbers <= '9'; numbers++)
+		/* Print current value  */
 		putchar(numbers);
-		numbers++;
-	}
+	/* Prints new line  */
 	putchar('\n');
+	/* End */
 	return (0);
 }
