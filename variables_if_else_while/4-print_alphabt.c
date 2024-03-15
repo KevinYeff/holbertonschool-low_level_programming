58f35ae807e3aa16e9c3b0336fd0ee29c6ebcdfb
KevinYeff <kevinyeff94@gmail.com>
2024-03-14 23:38:35 -0500

    refactoring: comments to explain code, loop changed to for loop, function definition improved

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -2,23 +2,24 @@
 /**
  * main - Function that prints the alphabet in lowercase
  *        except "q" and "e".
+ *        We also can use ASCII Values
  *
  * Return: (0).
  */
 int main(void)
 {
-	char alphalow;
-
-	alphalow = 'a';
-
-	while (alphalow <= 'z')
+	/* Declares char variable */
+	char alphalow = 'a';
+	/* Loop Conditionals */
+	for (; alphalow <= 'z'; alphalow++)
 	{
+		/* Main Condition */
 		if (alphalow != 'q' && alphalow != 'e')
-		{
+			/* Prints value except 'q' and 'e' */
 			putchar(alphalow);
-		}
 		alphalow++;
 	}
+	/* Prints new line */
 	putchar('\n');
 	return (0);
 }
