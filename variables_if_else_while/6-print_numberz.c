417287677326f815f3e83e3ee51cd7ee16aef667
KevinYeff <kevinyeff94@gmail.com>
2024-03-15 21:19:24 -0500

    refactoring: Comments added, while loop changed to for loop

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 /**
  * main - Prints all numbers of base 10 starting from "0".
+ *        Uses ASCII values.
  *
  * Return: (0).
  */
 int main(void)
 {
-	int numbersz;
-
-	numbersz = '0';
-
-	while (numbersz <= '9')
-	{
+	/* Declares int variable */
+	int numbersz = 48;
+	/* Defines loop */
+	for (; numbersz <= 57; numbersz++)
+		/*Prints current value*/
 		putchar(numbersz);
-		numbersz++;
-	}
-	putchar('\n');
+	/* Prints new line*/
+	putchar(10);
+	/* Ends */
 	return (0);
-
 }
