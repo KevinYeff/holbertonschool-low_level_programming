2880dbdf9ff80134d005b80958ff6e76201effac
KevinYeff <kevinyeff94@gmail.com>
2024-03-18 23:09:09 -0500

    refactoring: Code comments added, seting new values to avoid extra steps, while loop changed to for loops

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -1,40 +1,37 @@
 #include <stdio.h>
 /**
  * main - Prints all possible different combinations of three digits.
- * Return: Shoulc be 0.
+ *        Uses ASCII Values.
+ *
+ * Return: Should be 0.
  */
 int main(void)
 {
-	int pos1, pos2, pos3;
-
-	pos1 = 48;
-
-	while (pos1 <= 57)
+	/* Declares int variables */
+	int pos1 = 48, pos2, pos3;
+	/* Defines main/nested loops */
+	for (; pos1 <= 55; pos1++)
 	{
-		pos2 = 48;
-		while (pos2 <= 57)
+		for (pos2 = pos1 + 1; pos2 <= 56; pos2++)
 		{
-			pos3 = 48;
-			while (pos3 <= 57)
+			for (pos3 = pos2 + 1; pos3 <= 57; pos3++)
 			{
-				if ((pos1 < pos2) && pos2 < pos3)
+				/* Prints current values */
+				putchar(pos1);
+				putchar(pos2);
+				putchar(pos3);
+				/* Condition to match the output*/
+				if (pos1 != 55)
 				{
-					putchar(pos1);
-					putchar(pos2);
-					putchar(pos3);
-
-					if (pos1 != 55)
-					{
-						putchar(44);
-						putchar(32);
-					}
+					/* Prints chars ", " */
+					putchar(44);
+					putchar(32);
 				}
-				pos3++;
 			}
-			pos2++;
 		}
-		pos1++;
 	}
+	/* Prints new line */
 	putchar(10);
+	/* Ends */
 	return (0);
 }
