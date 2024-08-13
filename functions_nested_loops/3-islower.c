a25b520409f745e60b0147e5b206cae97fa2ba64
KevinYeff <kevinyeff94@gmail.com>
2024-08-12 19:00:02 -0500

    refactoring(3-islower.c): improved documentation and code logic

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
  * _islower - Function that checks for lowercase character.
- * @c: Character to be tested.
+ * @c: The passed character to be tested.
+ *
  * Return: (1) if c is lowercase otherwise returns (0).
+ *
+ * Consideration: We are not asked to print but check.
  */
+
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
-	else
+	if (!(c >= 97 && c <=122))
 		return (0);
+	return (1);
 }
