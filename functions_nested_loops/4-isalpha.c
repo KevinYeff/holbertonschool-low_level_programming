c48f3929218eafffab84b41130182a3d2412621f
KevinYeff <kevinyeff94@gmail.com>
2024-08-12 22:17:44 -0500

    refactoring(4-isalpha.c): Improved documentation and code logic, removed unnecessary function call

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,16 +1,17 @@
 #include "main.h"
+
 /**
  * _isalpha - Function that checks for an alphabetic character.
- * @c: Character to be tested.
+ * @c: The given character to be tested.
+ *
  * Return: (1) if c is a letter, lowercase or uppercase returns (0) otherwise.
+ *
+ * Consideration: We are not asked to print but check.
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-	{
 		return (1);
-	}
-	else
-		return (0);
-	_putchar(10);
+	return (0);
 }
