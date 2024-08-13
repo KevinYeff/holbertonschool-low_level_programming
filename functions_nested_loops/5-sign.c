6812622c057f47c65dec05d0b1382825a2759fba
KevinYeff <kevinyeff94@gmail.com>
2024-08-12 23:34:25 -0500

    refactoring(5-sign.c): Improved code reduction

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,11 +1,16 @@
 #include "main.h"
+
 /**
  * print_sign - Function that prints the sing of a number.
  * @n: Number to be tested.
+ *
  * Return: (1) and prints + if n is greater than zero.
  *         (0) and prints 0 if n is zero.
  *         (-1) and prints - if n is less than zero.
+ *
+ * Consideration: Don't forget to print the signs.
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
@@ -13,14 +18,11 @@ int print_sign(int n)
 		_putchar(43);
 		return (1);
 	}
-	else if (n == 0)
+	if (n == 0)
 	{
 		_putchar(48);
 		return (0);
 	}
-	else
-	{
-		_putchar(45);
-		return (-1);
-	}
+	_putchar(45);
+	return (-1);
 }
