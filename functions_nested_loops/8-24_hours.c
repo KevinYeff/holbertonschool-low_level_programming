58c5f941d1caac9aaf1238f32e4cee3002058226
KevinYeff <kevinyeff94@gmail.com>
2024-08-14 22:53:36 -0500

    refactoring(8-24_hours.c): Improved documentation and code logic Printing time logic extracted into a new function named _print_time(). Nested 'while' loops changed to the 'for' version.

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,28 +1,41 @@
 #include "main.h"
 
 /**
+ * _print_time - prints the format to match the output.
+ * @h: Variable corresponding to hours.
+ * @m: Variable corresponding to minutes.
+ *
+ * Return: void.
+ *
+ * Consideration: Use only _putchar() function.
+ */
+
+void _print_time(int h, int m)
+{
+	_putchar((h / 10) + 48);
+	_putchar((h % 10) + 48);
+	_putchar(58);
+	_putchar((m / 10) + 48);
+	_putchar((m % 10) + 48);
+	_putchar(10);
+}
+
+/**
  * jack_bauer - Prints every minute of the day starting from
  *              00:00 to 23:59.
+ *
  * Return: Should be 0.
+ *
+ * Consideration: Consider to extract the print logic separetly.
  */
+
 void jack_bauer(void)
 {
 	int h, m;
 
-	h = 0;
-	while (h <= 23)
+	for (h = 0; h < 24; h++)
 	{
-		m = 0;
-		while (m <= 59)
-		{
-			_putchar((h / 10) + '0');
-			_putchar((h % 10) + '0');
-			_putchar(58);
-			_putchar((m / 10) + '0');
-			_putchar((m % 10) + '0');
-			_putchar(10);
-			m++;
-		}
-		h++;
+		for (m = 0; m < 60; m++)
+			_print_time(h, m);
 	}
 }
