d80773ca21b925a0cf5e4a42bd96b3fde8205627
KevinYeff <kevinyeff94@gmail.com>
2024-08-16 00:03:35 -0500

    refactoring(9-times_table.c): Improved documentation and code logic. Code now uses an auxiliar function to print the 9 times table. 'while' nested loop changed to it's 'for' version.

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,41 +1,55 @@
 #include "main.h"
 
 /**
+ * prints_format - Function that recieves 2 integers.
+ * @factor2: Integer used to match the output when multiplying by 0.
+ * @product: Integer used to match the rest of the output.
+ *
+ * Return: void.
+ */
+
+void prints_format(int factor2, int product)
+{
+	if (factor2 == 0)
+	{
+		_putchar(48 + product);
+	}
+	else if (product <= 9)
+	{
+		_putchar(44);
+		_putchar(32);
+		_putchar(32);
+		_putchar(48 + product);
+	}
+	else
+	{
+		_putchar(44);
+		_putchar(32);
+		_putchar(48 + (product / 10));
+		_putchar(48 + (product % 10));
+	}
+}
+
+/**
  * times_table - Prints the 9 times table,
  *               starting with 0.
+ *
+ * Return: void.
+ *
+ * Considerations: Examine the output.
  */
+
 void times_table(void)
 {
 	int factor1, factor2, product;
 
-	factor1 = 0;
-	while (factor1 <= 9)
+	for (factor1 = 0; factor1 <= 9; factor1++)
 	{
-		factor2 = 0;
-		while (factor2 <= 9)
+		for (factor2 = 0; factor2 <= 9; factor2++)
 		{
 			product = factor1 * factor2;
-			if (factor2 == 0)
-			{
-				_putchar(product + '0');
-			}
-			else if (product >= 10)
-			{
-				_putchar(44);
-				_putchar(32);
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-			}
-			else
-			{
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
-				_putchar(product + '0');
-			}
-			factor2++;
+			prints_format(factor2, product);
 		}
-		factor1++;
 		_putchar(10);
 	}
 }
