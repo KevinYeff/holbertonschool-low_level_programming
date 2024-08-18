31319ae07319d4148e54909051089a7648bee958
KevinYeff <kevinyeff94@gmail.com>
2024-08-18 08:42:12 -0500

    update(100-times_table.c): code uses aux function, logic updated

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,10 +1,37 @@
 #include "main.h"
+
+/**
+ * print_table - Auxiliar function that prints the n times table
+ * @product: Integer used to match the output
+ */
+
+void print_table(int product)
+{
+	if (product >= 100 && product <= 999)
+	{
+		_putchar((product / 100) + 48);
+		_putchar(((product / 10) % 10) + 48);
+		_putchar((product % 10) + 48);
+	}
+	else if (product >= 10 && product <= 99)
+	{
+		_putchar(32);
+		_putchar((product / 10) + 48);
+		_putchar((product % 10) + 48);
+	}
+	else
+	{
+		_putchar(32);
+		_putchar(32);
+		_putchar(product + 48);
+	}
+}
 /**
  * print_times_table - Prints n times table, starting with 0.
  *                     if  n is greater than 15 or less than 0
  *                     the function should not print anything.
  * @n: Number of times table.
- * Return: (0).
+ * Return: void.
  */
 
 void print_times_table(int n)
@@ -23,26 +50,7 @@ void print_times_table(int n)
 				product = factor1 * factor2;
 				_putchar(44);
 				_putchar(32);
-				if (factor1 == 0 && factor2 == 0)
-					_putchar(product + 48);
-				else if (product >= 100 && product <= 999)
-				{
-					_putchar((product / 100) + 48);
-					_putchar(((product / 10) % 10) + 48);
-					_putchar((product % 10) + 48);
-				}
-				else if (product >= 10 && product <= 99)
-				{
-					_putchar(32);
-					_putchar((product / 10) + 48);
-					_putchar((product % 10) + 48);
-				}
-				else
-				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(product + 48);
-				}
+				print_table(product);
 				factor2++;
 			}
 			factor1++;
