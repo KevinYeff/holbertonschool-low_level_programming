72da207694eb40ca1048518183c72c2902c4bd8c
KevinYeff <kevinyeff94@gmail.com>
2024-08-19 20:24:01 -0500

    refactoring(103-fibonacci.c): Improved code logic

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -1,26 +1,41 @@
 #include <stdio.h>
+
+/**
+ * print_fibo - Auxiliar function.
+ */
+
+void print_fibo(void)
+{
+	unsigned long int num1, num2, sum, eva;
+	int i;
+
+	num1 = 0, num2 = 1, eva = 0;
+
+	for (i = 0; i <= 4000000; i++)
+	{
+		sum = num1 += num2;
+		num1 = num2;
+		num2 = sum;
+
+		if (num2 % 2 == 0 && num2 <= 4000000)
+		{
+			eva += num2;
+		}
+	}
+	printf("%ld\n", eva);
+}
+
 /**
  * main - Program that finds and prints the sum of the even-valued terms,
  *        followed by a new line.
  *        considering the terms in the Fibonacci sequence whose values do
  *        not exceed 4,000,000.
+ *
  * Return: (0).
  */
+
 int main(void)
 {
-	unsigned long num1, num2, sum, eva;
-
-	num1 = 0, num2 = 1, eva = 0;
-	while (1)
-	{
-		sum = num1 += num2;
-		num1 = num2;
-		num2 = sum;
-		if (num2 > 4000000)
-			break;
-		if (num2 <= 4000000 && num2 % 2 == 0)
-			eva = eva + num2;
-	}
-	printf("%ld\n", eva);
+	print_fibo();
 	return (0);
 }
