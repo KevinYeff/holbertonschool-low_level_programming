0bd7a9c2d5d00c70857b2bb5ff78bdd64a2c2907
KevinYeff <kevinyeff94@gmail.com>
2024-08-27 15:50:25 -0500

    refactoring(4-print_most_numbers.c): Lines in code reduced

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,18 @@
 #include "main.h"
+
 /**
  * print_most_numbers - Function that prints the numbers, from 0 to 9, followed
  *                      by a new line.
  */
+
 void print_most_numbers(void)
 {
-	int numbers = 48;
+	int numbers;
 
-	while (numbers <= 57)
+	for (numbers = 48; numbers <= 57; numbers++)
 	{
-		if (numbers != 50 && numbers != 52)
-		{
+		if (!(numbers == 50 || numbers == 52))
 			_putchar(numbers);
-		}
-		numbers++;
 	}
 	_putchar(10);
 }
