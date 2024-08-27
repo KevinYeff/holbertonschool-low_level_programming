ffb19881991f0446ec32476d77c4a1e859af545f
KevinYeff <kevinyeff94@gmail.com>
2024-08-27 12:04:02 -0500

    refactoring(3-print_numbers.c): Improved code reduction

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,15 @@
 #include "main.h"
+
 /**
  * print_numbers - Function that prints the numbers, from 0 to 9, followed by a
  *                  new line.
  */
+
 void print_numbers(void)
 {
-	int number = 48;
+	int number;
 
-	while (number <= 57)
-	{
+	for (number = 48; number <= 57; number++)
 		_putchar(number);
-		number++;
-	}
 	_putchar(10);
 }
