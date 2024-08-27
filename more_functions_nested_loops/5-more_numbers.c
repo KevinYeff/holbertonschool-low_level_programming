178d28cd4b25de48df8f71850025bc17649bbb9e
KevinYeff <kevinyeff94@gmail.com>
2024-08-27 16:38:29 -0500

    refactoring(5-more_numbers.c): Code uses a for nested loop now

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,26 +1,23 @@
 #include "main.h"
+
 /**
  * more_numbers - Function that prints 10 times the numbers,
  *                from 0 to 14, followed by a new line.
  */
+
 void more_numbers(void)
 {
 	int row, column;
 
-	row = 0;
-	while (row <= 9)
+	for (row = 0; row <= 9; row++)
 	{
-		column = 0;
-		while (column <= 14)
+		for (column = 0; column <= 14; column++)
 		{
 			if (column >= 10)
-			{
-				_putchar(column / 10 + '0');
-			}
-			_putchar(column % 10 + '0');
-			column++;
+				_putchar(48 + (column / 10));
+
+			_putchar(48 + (column % 10));
 		}
 		_putchar(10);
-		row++;
 	}
 }
