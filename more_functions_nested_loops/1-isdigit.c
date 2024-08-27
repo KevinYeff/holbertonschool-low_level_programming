571583ea4762d9ffd31d20e76163371ec3ca2508
KevinYeff <kevinyeff94@gmail.com>
2024-08-27 11:16:09 -0500

    update(1-isdigit.c): Code now uses the given digit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,15 @@
 #include "main.h"
+
 /**
  * _isdigit - Function that checks for a digit (0 trought 9).
  * @c: Digit to be tested.
  * Return: (1) if "c" is a digit, return (0) otherwise.
  */
+
 int _isdigit(int c)
 {
-	int digit;
-
-	digit = c;
-	if (digit >= 48 && digit <= 57)
-	{
+	if (c >= 48 && c <= 57)
 		return (1);
-	}
 	else
 		return (0);
 }
