8049da0250b5b3491e816ae5ccabbc46e17b6373
KevinYeff <kevinyeff94@gmail.com>
2024-08-27 17:11:07 -0500

    refactoring(6-print_line): Improved code reduction

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,18 +1,17 @@
 #include "main.h"
+
 /**
  * print_line - Function that draws a straight line in the terminal.
  * @n: Number of times the characer "_" should be printed.
  *               The line should end with a "\n".
  *               If n is 0 or less, the function should only print "\n".
  */
+
 void print_line(int n)
 {
-	int pline = 0;
+	int pline;
 
-	while (pline < n)
-	{
+	for (pline = 0; pline < n; pline++)
 		_putchar(95);
-		pline++;
-	}
 	_putchar(10);
 }
