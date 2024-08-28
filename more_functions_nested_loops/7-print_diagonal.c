f1881ad8658579d2239cfba78d51799fbc0b1f56
KevinYeff <kevinyeff94@gmail.com>
2024-08-28 07:33:55 -0500

    refactoring(7-print_diagonal.c): Improved logic and code reduction Code now uses a nested for loop. Lines of code reduced according to betty linter rules.

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,26 +1,22 @@
 #include "main.h"
+
 /**
  * print_diagonal - Function that draws a diagonal line on the
  *                  terminal.
  * @n: The number of times the character "\" should be printed.
  */
+
 void print_diagonal(int n)
 {
 	int spaces, bslash;
 
-	bslash = 0;
-	while (bslash < n)
+	for (bslash = 0; bslash < n; bslash++)
 	{
-		spaces = 0;
-		while (spaces < bslash)
-		{
+		for (spaces = 0; spaces < bslash; spaces++)
 			_putchar(32);
-			spaces++;
-		}
 		_putchar(92);
 		_putchar(10);
-		bslash++;
 	}
 	if (n <= 0)
-	_putchar(10);
+		_putchar(10);
 }
