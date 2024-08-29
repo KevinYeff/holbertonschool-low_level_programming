557b3b1793d68d840b517e29813c91980e20fbab
KevinYeff <kevinyeff94@gmail.com>
2024-08-28 21:12:05 -0500

    refactoring(10-print_triangle.c): Improved code reduction

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,34 +1,26 @@
 #include "main.h"
+
 /**
  * print_triangle - Function that prints a triangle.
  *                  folowed by a new line.
  * @size: Size of the triangle.
  */
+
 void print_triangle(int size)
 {
 	int row, column;
 
-	row = 1;
-	while (row <= size)
+	for (row = 1; row <= size; row++)
 	{
-		column = 1;
-		while (column <= size)
+		for (column = 1; column <= size; column++)
 		{
 			if (column <= (size - row))
-			{
 				_putchar(32);
-			}
 			else
-			{
 				_putchar(35);
-			}
-			column++;
 		}
 		_putchar(10);
-		row++;
 	}
 	if (size <= 0)
-	{
 		_putchar(10);
-	}
 }
